d80c2bcf96ce88b19d7d3a259ebbd2060aede80f
Evan K <ekerik220@gmail.com>
2020-04-06 16:11:40 +0900

    Updates

---
+++ precache-manifest.157106656a2a233e349ee9a6592786e2.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d34bea0417962460401525b93e39a3c",
+    "url": "/First-Trend-Clothing/index.html"
+  },
+  {
+    "revision": "d908fdbdb33032ae2073",
+    "url": "/First-Trend-Clothing/static/css/2.a35bfe2f.chunk.css"
+  },
+  {
+    "revision": "cfea9d8a0adfe99717cc",
+    "url": "/First-Trend-Clothing/static/css/main.98abbacb.chunk.css"
+  },
+  {
+    "revision": "d908fdbdb33032ae2073",
+    "url": "/First-Trend-Clothing/static/js/2.a80fa12e.chunk.js"
+  },
+  {
+    "revision": "1ec985a6acf507610a7e29537bfcee7f",
+    "url": "/First-Trend-Clothing/static/js/2.a80fa12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfea9d8a0adfe99717cc",
+    "url": "/First-Trend-Clothing/static/js/main.cd463b33.chunk.js"
+  },
+  {
+    "revision": "a9832a032dca4449be4d",
+    "url": "/First-Trend-Clothing/static/js/runtime-main.eea905e8.js"
+  },
+  {
+    "revision": "90c6a4d591fc3da3f69886785a40d60a",
+    "url": "/First-Trend-Clothing/static/media/blue-jacket.90c6a4d5.jpg"
+  },
+  {
+    "revision": "a33bfe020d9322e1ccdc04662d14b37b",
+    "url": "/First-Trend-Clothing/static/media/blue-longslv.a33bfe02.jpg"
+  },
+  {
+    "revision": "34960708592c8c3757f9b19d349a79e6",
+    "url": "/First-Trend-Clothing/static/media/blue-poloshirt.34960708.jpg"
+  },
+  {
+    "revision": "2835233086b469b8835c7cf68776491c",
+    "url": "/First-Trend-Clothing/static/media/blue-sweatshirt.28352330.jpg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/First-Trend-Clothing/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/First-Trend-Clothing/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/First-Trend-Clothing/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/First-Trend-Clothing/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/First-Trend-Clothing/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/First-Trend-Clothing/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/First-Trend-Clothing/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/First-Trend-Clothing/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/First-Trend-Clothing/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/First-Trend-Clothing/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/First-Trend-Clothing/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/First-Trend-Clothing/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/First-Trend-Clothing/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/First-Trend-Clothing/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/First-Trend-Clothing/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "63f86b8c0ae098b2b986035e3f53e55d",
+    "url": "/First-Trend-Clothing/static/media/green-jacket.63f86b8c.jpg"
+  },
+  {
+    "revision": "6744b898a8a37404e0b877d312ca50be",
+    "url": "/First-Trend-Clothing/static/media/green-longslv.6744b898.jpg"
+  },
+  {
+    "revision": "d2fcfb022870f98c69c7892243e85cb9",
+    "url": "/First-Trend-Clothing/static/media/green-poloshirt.d2fcfb02.jpg"
+  },
+  {
+    "revision": "accd92df86c9dbc29effe4adc7e80149",
+    "url": "/First-Trend-Clothing/static/media/green-sweatshirt.accd92df.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-Trend-Clothing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "67b1fa4bb59ceef9026a18c7bad8529c",
+    "url": "/First-Trend-Clothing/static/media/men_clothing.67b1fa4b.jpg"
+  },
+  {
+    "revision": "d835b08765105cf16f3c22050d15b0a5",
+    "url": "/First-Trend-Clothing/static/media/orange-jacket.d835b087.jpg"
+  },
+  {
+    "revision": "b630591a8982bcae4db2a6438ddb11e0",
+    "url": "/First-Trend-Clothing/static/media/orange-longslv.b630591a.jpg"
+  },
+  {
+    "revision": "66bd349dfbfa7f3749c3952ab59244b5",
+    "url": "/First-Trend-Clothing/static/media/orange-poloshirt.66bd349d.jpg"
+  },
+  {
+    "revision": "62eb3976803f0d052147f8f452a5a094",
+    "url": "/First-Trend-Clothing/static/media/orange-sweatshirt.62eb3976.jpg"
+  },
+  {
+    "revision": "157712e458ca64aa38ad6893abfb3094",
+    "url": "/First-Trend-Clothing/static/media/pink-jacket.157712e4.jpg"
+  },
+  {
+    "revision": "1fea8d33e952702a308f0378e9919c5c",
+    "url": "/First-Trend-Clothing/static/media/pink-longslv.1fea8d33.jpg"
+  },
+  {
+    "revision": "f5d9dbe5d5934538ca0a3a0ecc816e5a",
+    "url": "/First-Trend-Clothing/static/media/pink-poloshirt.f5d9dbe5.jpg"
+  },
+  {
+    "revision": "008d5976169d2fe425b8b168b8146820",
+    "url": "/First-Trend-Clothing/static/media/pink-sweatshirt.008d5976.jpg"
+  },
+  {
+    "revision": "1e2d1f9d65b78ad71b68b5faa45cfc88",
+    "url": "/First-Trend-Clothing/static/media/purple-jacket.1e2d1f9d.jpg"
+  },
+  {
+    "revision": "7fbe4979f631d9ceaba96f6cc0b92624",
+    "url": "/First-Trend-Clothing/static/media/purple-longslv.7fbe4979.jpg"
+  },
+  {
+    "revision": "629b6b0c690d20680dba9661289d9a10",
+    "url": "/First-Trend-Clothing/static/media/purple-poloshirt.629b6b0c.jpg"
+  },
+  {
+    "revision": "d55601c2c0294b210644c4c646e2913d",
+    "url": "/First-Trend-Clothing/static/media/purple-sweatshirt.d55601c2.jpg"
+  },
+  {
+    "revision": "df0815218af4c220ad8492de63900ef5",
+    "url": "/First-Trend-Clothing/static/media/red-jacket.df081521.jpg"
+  },
+  {
+    "revision": "392ffd19f159d7272ef44cbd34188b3b",
+    "url": "/First-Trend-Clothing/static/media/red-longslv.392ffd19.jpg"
+  },
+  {
+    "revision": "55c8b77618298e7df9f8d6bf4cedc864",
+    "url": "/First-Trend-Clothing/static/media/red-poloshirt.55c8b776.jpg"
+  },
+  {
+    "revision": "cc816d9568287463fe79733fbb60a75f",
+    "url": "/First-Trend-Clothing/static/media/red-sweatshirt.cc816d95.jpg"
+  },
+  {
+    "revision": "ea388e50627a60ef62980b79b9db132f",
+    "url": "/First-Trend-Clothing/static/media/stock-shirt.ea388e50.jpg"
+  },
+  {
+    "revision": "b1f52c707b217ce32c26523391e1ccd5",
+    "url": "/First-Trend-Clothing/static/media/yellow-jacket.b1f52c70.jpg"
+  },
+  {
+    "revision": "2e0e65fa5018602f337c2c1c5e3ea8e6",
+    "url": "/First-Trend-Clothing/static/media/yellow-longslv.2e0e65fa.jpg"
+  },
+  {
+    "revision": "40b31d9cc971ecae483e5a21183a980c",
+    "url": "/First-Trend-Clothing/static/media/yellow-poloshirt.40b31d9c.jpg"
+  },
+  {
+    "revision": "fd86e0c8b69f00131d6577e5b977f931",
+    "url": "/First-Trend-Clothing/static/media/yellow-sweatshirt.fd86e0c8.jpg"
+  }
+]);
